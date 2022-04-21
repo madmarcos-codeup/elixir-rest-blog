5f6ff01e46fb2fcc38bd22d127d64ba3dfea4066
madmarcos-codeup <mark.robinson@codeup.com>
2022-04-20 21:35:10 -0500

    data persist done

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,31 +1,22 @@
 package com.example.restblog.web;
 
-import com.example.restblog.data.Category;
-import com.example.restblog.data.Post;
-import com.example.restblog.data.PostsRepository;
-import com.example.restblog.data.User;
+import com.example.restblog.data.*;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @CrossOrigin
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 public class PostsController {
     private final PostsRepository postRepository;
-
-    private static final User USER1 = new User(1L, "User 1", "user1@bob.com", "1111", null, User.Role.USER, null);
-    private static final User USER2 = new User(2L, "User 2", "user2@bob.com", "2222", null, User.Role.ADMIN, null);
-    private static final User USER3 = new User(3L, "User 3", "user3@bob.com", "3333", null, User.Role.USER, null);
-
-    private static final Category CAT1 = new Category(1L, "CAT 1", null);
-    private static final Category CAT2 = new Category(2L, "CAT 2", null);
-    private static final Category CAT3 = new Category(3L, "CAT 3", null);
-
-    public PostsController(PostsRepository postRepository) {
-        this.postRepository = postRepository;
-    }
+    private final UsersRepository userRepository;
+    private final CategoriesRepository categoriesRepository;
 
     @GetMapping
     private List<Post> getAll() {
@@ -39,17 +30,44 @@ public class PostsController {
 
     @PostMapping
     private void createPost(@RequestBody Post newPost) {
-        System.out.println("Backend wants to create: " + newPost);
+        // associate user 1 with the post
+        // also plug in all categories (1, 2, 3)
+        newPost.setAuthor(userRepository.getById(1L));
+        newPost.setCategories(Arrays.asList(categoriesRepository.getById(1L),
+                categoriesRepository.getById(2L),
+                categoriesRepository.getById(3L)));
+        postRepository.save(newPost);
     }
 
     @PutMapping("{postId}")
     private void updatePost(@PathVariable Long postId, @RequestBody Post newPost) {
         System.out.printf("Backend wants to update post id %d with %s\n", postId, newPost);
+        Post originalPost = postRepository.getById(postId);
+        BeanUtils.copyProperties(newPost, originalPost, getNullPropertyNames(newPost));
+        postRepository.save(originalPost);
     }
 
     @DeleteMapping("{postId}")
     private void deletePost(@PathVariable Long postId) {
         System.out.printf("Backend wants to delete post id %d\n", postId);
+        postRepository.deleteById(postId);
     }
+
+    // CONVENIENCE METHOD TO AVOID NULL CHECKS FOR EVERY PROPERTY ON UPDATE
+    // thank you Ry!!
+    private static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<String>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
 
 }
--- src/main/java/com/example/restblog/web/UsersController.java
+++ src/main/java/com/example/restblog/web/UsersController.java
@@ -13,17 +13,6 @@ import java.util.Optional;
 public class UsersController {
     private final UsersRepository userRepository;
 
-    private static final Category CAT1 = new Category(1L, "CAT 1", null);
-    private static final Category CAT2 = new Category(2L, "CAT 2", null);
-    private static final Category CAT3 = new Category(3L, "CAT 3", null);
-
-    private static final Post POST1 = new Post(1L, "Post 1", "Blah", null, Arrays.asList(CAT1, CAT2));
-    private static final Post POST2 = new Post(2L, "Post 2", "Blah", null, Arrays.asList(CAT2, CAT3));
-    private static final Post POST3 = new Post(3L, "Post 3", "Blah", null, Arrays.asList(CAT1, CAT3));
-    private static final Post POST4 = new Post(4L, "Post 4", "Blah", null, Arrays.asList(CAT1, CAT2));
-    private static final Post POST5 = new Post(5L, "Post 5", "Blah", null, Arrays.asList(CAT2, CAT3));
-    private static final Post POST6 = new Post(6L, "Post 6", "Blah", null, Arrays.asList(CAT1, CAT3));
-
     public UsersController(UsersRepository userRepository) {
         this.userRepository = userRepository;
     }
